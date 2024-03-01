c5250e9b8a15064d85c114eeac0df45d674e0e89
enricoforesta <enrico.foresta97@gmail.com>
2024-03-01 15:54:28 +0100

    feat: migrations order

--- database/migrations/2024_03_01_134800_create_orders_table.php
+++ database/migrations/2024_03_01_134800_create_orders_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('restaurant_id')->constrained()->cascadeOnDelete();
+            $table->dateTime('date')->nullable();
+            $table->string('customer_name', 50);
+            $table->string('email', 50);
+            $table->string('delivery_address', 100)->nullable();
+            $table->decimal('final_price', 5, 2)->unsigned()->nullable();
             $table->timestamps();
         });
     }
