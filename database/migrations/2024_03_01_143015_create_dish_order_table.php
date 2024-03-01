4b27837c568baf48bb133f45110a79e21fd8a6cc
SimoneCalzari <calzarisimone@gmail.com>
2024-03-01 15:44:34 +0100

    feat: tabelle pivot

---
+++ database/migrations/2024_03_01_143015_create_dish_order_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('dish_order', function (Blueprint $table) {
+            // $table->id();
+            $table->foreignId('dish_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('order_id')->constrained()->cascadeOnDelete();
+            $table->tinyInteger('quantity')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dish_order');
+    }
+};
