ec0c5b07445dcadff15b6f83c3469c8a7a242eb1
SimoneCalzari <calzarisimone@gmail.com>
2024-03-01 16:51:42 +0100

    feat: seeder type

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,55 @@ class TypeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $types =  [
+            [
+                "name" => "Sushi",
+                "image" => "public\img\sushi.jpg",
+            ],
+            [
+                "name" => "Fast Food",
+                "image" => "public\img\sushi.jpg",
+            ],
+            [
+                "name" => "Pizza",
+                "image" => "public\img\sushi.jpg",
+            ],
+            [
+                "name" => "Cinese",
+                "image" => "public\img\sushi.jpg",
+            ],
+            [
+                "name" => "Messicano",
+                "image" => "public\img\sushi.jpg",
+            ],
+            [
+                "name" => "Italiano",
+                "image" => "public\img\sushi.jpg",
+            ],
+            [
+                "name" => "Hamburger",
+                "image" => "public\img\sushi.jpg",
+            ],
+            [
+                "name" => "Caffè",
+                "image" => "public\img\sushi.jpg",
+            ],
+            [
+                "name" => "Gelateria",
+                "image" => "public\img\sushi.jpg",
+            ],
+            [
+                "name" => "Vegetariano",
+                "image" => "public\img\sushi.jpg",
+            ],
+        ];
+
+        foreach ($types as $type) {
+
+            $type_new = new Type();
+            $type_new->name_type = $type['name'];
+            $type_new->image = $type['image'];
+            $type_new->save();
+        }
     }
 }
