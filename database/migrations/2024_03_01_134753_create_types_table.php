588e2059f2b12d98aac43013836261a62abf0157
elisabichiappini <elisabichiappini@gmail.com>
2024-03-01 15:53:31 +0100

    feat: migration types

--- database/migrations/2024_03_01_134753_create_types_table.php
+++ database/migrations/2024_03_01_134753_create_types_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('types', function (Blueprint $table) {
             $table->id();
+            $table->string('name_type', 60);
+            $table->string('image', 255);
             $table->timestamps();
         });
     }
