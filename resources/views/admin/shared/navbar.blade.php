9573b5d0e393ede1958836ad91ddc6228136bed1
SimoneCalzari <calzarisimone@gmail.com>
2024-03-06 11:24:47 +0100

    feat: bho

--- resources/views/admin/shared/navbar.blade.php
+++ resources/views/admin/shared/navbar.blade.php
@@ -2,7 +2,7 @@
     <div class="row justify-content-between">
         @foreach ($restaurants_passare as $restaurant)
             <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="{{ route('admin.dashboard') }}">
-                {{ $restaurant->name_restaurant }}
+                Benvenuto, {{ $restaurant->user->name }}
             </a>
         @endforeach
         <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse"
@@ -12,7 +12,7 @@
     </div>
     <div class="navbar-nav">
         <div class="nav-item text-nowrap ms-2">
-            <a class="nav-link pe-2" href="{{ route('logout') }}"
+            <a class="nav-link pe-2 text-white" href="{{ route('logout') }}"
                 onclick="event.preventDefault();
         document.getElementById('logout-form').submit();">
                 {{ __('Esci') }}
