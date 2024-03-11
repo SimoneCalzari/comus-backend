82d6a03adbe38a5abf2b67b2ec653e759aa16a59
SimoneCalzari <calzarisimone@gmail.com>
2024-03-11 18:12:02 +0100

    feat: order controller api primo tentativo

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -8,14 +8,28 @@ use App\Models\Order;
 
 class OrderController extends Controller
 {
-    public function store(StoreOrderRequest $request)
+    // public function store(StoreOrderRequest $request)
+    public function store()
     {
-
+        //  {
+        //     formData: this.formData,
+        //     cart: this.store.cart,
+        //     totalPrice: this.store.totalPrice,
+        //   };
+        $data = request()->all();
         $order = new Order();
-        $order->fill($request->all());
+        $order->restaurant_id = $data['cart'][0]['restaurant_id'];
+        $order->customer_name = $data['formData']['customer_name'];
+        $order->email = $data['formData']['email'];
+        $order->delivery_address = $data['formData']['delivery_address'];
+        $order->final_price = $data['totalPrice'];
         $order->save();
-        return response()->json([
-            'results' => $order
-        ]);
+
+        $dishes_ids = [];
+        foreach ($data['cart'] as $dish) {
+            array_push($dishes_ids, $dish->id);
+        }
+
+        $order->dishes()->sync($dishes_ids);
     }
 }
