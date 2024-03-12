eb0cd2e89bf0a7ededffc3b475c6007c6f8ac77a
SimoneCalzari <calzarisimone@gmail.com>
2024-03-12 18:00:39 +0100

    feat: struttura mail per utente consumatore

--- resources/views/mails/new-order-mail.blade.php
+++ resources/views/mails/new-order-mail.blade.php
@@ -1,13 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Mail</title>
 </head>
+
 <body>
-    <h2>{{ $lead->customer_name}}</h2>
-    <h2>{{ $lead->delivery_address}}</h2>
+    <p>Gentile {{ $lead->customer_name }},</p>
+    <p>Il suo ordine che comprende:</p>
+    <ul>
+        @foreach ($lead->dishes as $dish)
+            <li>{{ $dish->name }} - {{ $dish->pivot->quantity }}</li>
+        @endforeach
+    </ul>
+    <p>Per un totale di {{ $lead->final_price }} € è stato accettato da {{ $lead->restaurant->name_restaurant }} e le
+        verrà consegnato all'indirizzo ed orario da lei indicato al momento dell'ordine.</p>
+    <p>Il team di Comus le augura una buona serata e una buona cena a domicilio &hearts;</p>
 </body>
+
 </html>
